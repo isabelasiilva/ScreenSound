9d2cb6532ace7e1e3831a08aad5ff055a5610aaa
BOMBEIROSSP\51057172871 <isabelacristina@policiamilitar.sp.gov.br>
2025-09-24 22:32:56 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Modelos/Artista.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+
+namespace ScreenSound.Modelos; 
+
+internal class Artista 
+{
+    private List<Musica> musicas = new List<Musica>();
+
+    public Artista(string nome, string bio)
+    {
+        Nome = nome;
+        Bio = bio;
+        FotoPerfil = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
+    }
+    public Artista(int id)
+    {
+        Id = id;        
+    }
+
+    public Artista(int id, string nome, string bio)
+    {
+        Id = id;
+        Nome = nome;
+        Bio = bio;
+        FotoPerfil = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
+    }
+
+    public string? Nome { get; set; }
+    public string? FotoPerfil { get; set; }
+    public string? Bio { get; set; }
+    public int Id { get; set; }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirDiscografia()
+    {
+        Console.WriteLine($"Discografia do artista {Nome}");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $@"Id: {Id}
+            Nome: {Nome}
+            Foto de Perfil: {FotoPerfil}
+            Bio: {Bio}";
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,88 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+try 
+{
+    //var connection = new Connection(); não precisa mais pq ja ta sendo usado dentro dos metodos em ArtistaDAL
+
+    var artistaDAL = new ArtistaDAL();
+
+    // criando um artista novo
+    //artistaDAL.Adicionar(new Artista("Nirvana", "Banda de rock dos anos 90"));
+
+    // atualizando um artista
+    //artistaDAL.Atualizar(new Artista(6, "Nirvana 2", "Banda de rock dos anos 90"));
+
+    // deletando um artista
+    artistaDAL.Deletar(new Artista(6));
+
+    // listando os artistas que estao no banco
+    var listaArtistas = artistaDAL.Listar();
+
+    foreach (var artista in listaArtistas)
+    {
+        Console.WriteLine(artista);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
+
+
+Artista ira = new Artista("Ira!", "Banda Ira!");
+Artista beatles = new("The Beatles", "Banda The Beatles");
+
+Dictionary<string, Artista> artistasRegistrados = new();
+artistasRegistrados.Add(ira.Nome, ira);
+artistasRegistrados.Add(beatles.Nome, beatles);
+
+Dictionary<int, Menu> opcoes = new();
+opcoes.Add(1, new MenuRegistrarArtista());
+opcoes.Add(2, new MenuRegistrarMusica());
+opcoes.Add(3, new MenuMostrarArtistas());
+opcoes.Add(4, new MenuMostrarMusicas());
+opcoes.Add(-1, new MenuSair());
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar um artista");
+    Console.WriteLine("Digite 2 para registrar a música de um artista");
+    Console.WriteLine("Digite 3 para mostrar todos os artistas");
+    Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+    {
+        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(artistasRegistrados);
+        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+    } 
+    else
+    {
+        Console.WriteLine("Opção inválida");
+    }
+}
+
+ExibirOpcoesDoMenu();
