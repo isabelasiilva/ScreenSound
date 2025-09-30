c6d61143f81549e6ed76d0f0d82094e11fc5804a
BOMBEIROSSP\51057172871 <isabelacristina@policiamilitar.sp.gov.br>
2025-09-29 22:33:46 -0300

    Abstraindo ArtistaDAL e MusicaDAL para o DAL usando generics

---
+++ ScreenSound/Banco/DAL.cs
@@ -0,0 +1,31 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    // Criando um DAL genérico porque Artista e Musica possuem os mesmos comportamentos
+    internal abstract class DAL<T>
+    {
+        private readonly ScreenSoundContext context; // esse campo adiciona no construtor do artistaDAL
+
+        public DAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<T> Listar()
+        {
+            return context.<DAL>.ToList();
+        }
+        public abstract void Adicionar(T objeto);
+        public abstract void Atualizar(T objeto);
+        public abstract void Deletar(T objeto);
+
+   
+    }
+
+}
